0e895073d94e424531fcbf50fb5d0d5c41baed61
amrakibe <aminerakibe7@gmail.com>
2025-04-11 16:53:20 +0100

    final merge

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -28,16 +28,25 @@ wp config create --allow-root --skip-check \
 
 # Creates the WordPress tables in the database using the detected IP
 echo "Installing WordPress core with server IP: $SERVER_IP"
-wp core install \
-    --url="https://$SERVER_IP" \
-    --title="ATABITI INCEPTION" \
-    --admin_user=$WORDPRESS_ADMIN \
-    --admin_password=$WORDPRESS_PASSWORD_ADMIN \
-    --admin_email="atabiti@student.1337.ma" --allow-root
+# wp core install \
+#     --url="https://$SERVER_IP" \
+#     --title="My Automated WordPress Site" \
+#     --admin_user=$WORDPRESS_ADMIN \
+#     -admin_email=$ADMIN_WP_EMAIL \
+#     --allow-root
+
+    wp core install \
+        --url="https://$SERVER_IP" \
+        --title="My Automated WordPress Site" \
+        --admin_user="$WORDPRESS_ADMIN" \
+        --admin_password="$WORDPRESS_PASSWORD_ADMIN" \
+        --admin_email="$ADMIN_WP_EMAIL" \
+        --skip-email \
+        --allow-root
 
 # Creates a new user
 echo "Creating additional user..."
-wp user create $WORDPRESS_USER "anas@$SERVER_IP" \
+wp user create $WORDPRESS_USER "member@$SERVER_IP" \
     --allow-root \
     --role=editor \
     --user_pass=$WORDPRESS_PASSWORD
@@ -61,36 +70,3 @@ echo "WordPress configuration completed"
 echo "Starting PHP-FPM..."
 exec php-fpm
 
-# cd  /Cloud/wordpress && wp config create	--allow-root --skip-check \
-# 	 --dbname=$MYSQL_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD \
-# 	  --dbhost=$MYSQL_HOST --path=/Cloud/wordpress
-# ################################################################################
-# #2 Creates the WordPress tables in the database using the URL, title, 
-# # and default admin user details provided. Performs the famous 5 minute install in seconds or less.
-
-# cd  /Cloud/wordpress &&  wp core install \
-# 		--url="https://localhost"  \
-# 		--title="ATABITI INCEPTION" \
-# 		--admin_user=$WORDPRESS_ADMIN  \
-# 		--admin_password=$WORDPRESS_PASSWORD_ADMIN \
-# 		--admin_email="atabiti@student.1337.ma" --allow-root
-# ###############################################################################
-# #3 Creates a new user.
-# cd  /Cloud/wordpress &&  wp user create $WORDPRESS_USER	anas@localhost \
-# 					--allow-root \
-# 					--role=editor \
-# 					--user_pass=$WORDPRESS_PASSWORD
-# /usr/sbin/php-fpm7.3 -F
-
-
-
-
-################################# MANUAL  ############################################
-#--dbname=<dbname>  Set the database name.
-#--dbuser=<dbuser>   Set the database user.
-# [--dbpass=<dbpass>] Set the database user password.
-#[--dbhost=<dbhost>] Set the database host.
-#--skip-check If set, the database connection is not checked.
-
-#-F, --nodaemonize
- #                  force to stay in foreground, and ignore daemonize option from config file
